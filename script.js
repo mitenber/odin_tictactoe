897a60c68d82661e146bf4bcd6529d68903e184d
Mikhail <mitenber@gmail.com>
2025-05-03 17:40:30 -0400

    First commit, basic skeleton for project

---
+++ script.js
@@ -0,0 +1,21 @@
+const startBtn = document.querySelector("#start-button");
+startBtn.addEventListener("click", () => {
+    alert("Hello Asshole");
+})
+
+
+function gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+
+}
+
+function player() {
+
+}
+
+function game() {
+
+}
